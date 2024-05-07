8f9b9488fe083424a5ad5087d12b86689f5bec95
Wahdan Najmil <najmilwahdan@gmail.com>
2024-05-07 10:55:20 +0700

    change session status

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,11 +2,14 @@
 class Home extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->helper('url'); // Memuat URL Helper
+        $this->load->helper('url'); 
         $this->load->library('session');
     }
 
     public function index() {
+        if (!$this->session->userdata('logged_in')) {
+            redirect('auth/login');
+        }
         $this->load->view('home_view');
     }
 }
